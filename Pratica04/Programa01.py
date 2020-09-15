974e35ce84d0339e9ad79d24d618014a649c0b81
Leandro-eusebio <70581511+Leandro-eusebio@users.noreply.github.com>
2020-09-15 08:13:09 -0300

    Create Programa01.py

---
+++ Pratica04/Programa01.py
@@ -0,0 +1,11 @@
+#pede ao usuáriro para digitar um valor para idade
+idade = int(input('informe sua idade:'))
+#Pergunta, no código se a idade é maior ou igual a 18
+if idade >= 18:
+ 
+ #este código pertence ao bloco do if
+ print('Você possui 18 anos ou mais')
+ print ('Agora! Você pode ver todas as páginas de jogos da Steam')
+
+
+print('Final do programa')
